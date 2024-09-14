5795f713a3aa6db30d18323081dcbd6e584b0dde
Mateus Vagner <mateus.vagner@gmail.com>
2024-09-14 19:47:32 -0300

    refactor: improve data error extensions and add new error

--- app/src/main/kotlin/com/mv/acessif/presentation/DataErrorExtension.kt
+++ app/src/main/kotlin/com/mv/acessif/presentation/DataErrorExtension.kt
@@ -23,6 +23,7 @@ fun DataError.asUiText(): UiText {
         is DataError.Local -> {
             when (this) {
                 DataError.Local.PERMISSION_DENIED -> UiText.StringResource(R.string.permission_denied)
+                DataError.Local.FILE_NOT_FOUND -> UiText.StringResource(R.string.file_not_found)
             }
         }
     }
