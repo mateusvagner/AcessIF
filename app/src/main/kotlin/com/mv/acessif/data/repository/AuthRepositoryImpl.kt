bb36dd3f4e197938d118ea844faace30fae8c970
Mateus Vagner <mateus.vagner@gmail.com>
2024-10-16 23:44:00 -0300

    refactor: add dispatchers

--- app/src/main/kotlin/com/mv/acessif/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/kotlin/com/mv/acessif/data/repository/AuthRepositoryImpl.kt
@@ -13,6 +13,8 @@ import com.mv.acessif.domain.repository.AuthRepository
 import com.mv.acessif.domain.returnModel.DataError
 import com.mv.acessif.domain.returnModel.Result
 import com.mv.acessif.network.service.AuthService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class AuthRepositoryImpl
@@ -22,39 +24,45 @@ class AuthRepositoryImpl
     ) : AuthRepository {
         override suspend fun login(login: Login): Result<AuthToken, DataError.Network> {
             val loginRequestDto = LoginMapper.mapLoginToLoginRequestDto(login)
-            return try {
-                val authTokenDto = authService.postLogin(loginRequestDto)
-                val authToken = AuthTokenMapper.mapAuthTokenDtoToAuthToken(authTokenDto)
-                Result.Success(authToken)
-            } catch (e: Exception) {
-                Result.Error(
-                    ErrorMapper.mapNetworkExceptionToNetworkDataError(e),
-                )
+            return withContext(Dispatchers.IO) {
+                try {
+                    val authTokenDto = authService.postLogin(loginRequestDto)
+                    val authToken = AuthTokenMapper.mapAuthTokenDtoToAuthToken(authTokenDto)
+                    Result.Success(authToken)
+                } catch (e: Exception) {
+                    Result.Error(
+                        ErrorMapper.mapNetworkExceptionToNetworkDataError(e),
+                    )
+                }
             }
         }
 
         override suspend fun signUp(signUp: SignUp): Result<AuthToken, DataError.Network> {
             val signUpRequestDto = SignUpMapper.mapSignUpToSignUpRequestDto(signUp)
-            return try {
-                val authTokenDto = authService.postSignUp(signUpRequestDto)
-                val authToken = AuthTokenMapper.mapAuthTokenDtoToAuthToken(authTokenDto)
-                Result.Success(authToken)
-            } catch (e: Exception) {
-                Result.Error(
-                    ErrorMapper.mapNetworkExceptionToNetworkDataError(e),
-                )
+            return withContext(Dispatchers.IO) {
+                try {
+                    val authTokenDto = authService.postSignUp(signUpRequestDto)
+                    val authToken = AuthTokenMapper.mapAuthTokenDtoToAuthToken(authTokenDto)
+                    Result.Success(authToken)
+                } catch (e: Exception) {
+                    Result.Error(
+                        ErrorMapper.mapNetworkExceptionToNetworkDataError(e),
+                    )
+                }
             }
         }
 
         override suspend fun refreshToken(refreshToken: String): Result<AccessToken, DataError.Network> {
-            return try {
-                val accessTokenDto = authService.postRefreshToken(refreshToken)
-                val accessToken = AccessTokenMapper.mapAccessTokenDtoToAccessToken(accessTokenDto)
-                Result.Success(accessToken)
-            } catch (e: Exception) {
-                Result.Error(
-                    ErrorMapper.mapNetworkExceptionToNetworkDataError(e),
-                )
+            return withContext(Dispatchers.IO) {
+                try {
+                    val accessTokenDto = authService.postRefreshToken(refreshToken)
+                    val accessToken = AccessTokenMapper.mapAccessTokenDtoToAccessToken(accessTokenDto)
+                    Result.Success(accessToken)
+                } catch (e: Exception) {
+                    Result.Error(
+                        ErrorMapper.mapNetworkExceptionToNetworkDataError(e),
+                    )
+                }
             }
         }
     }
