762b98f57d9f3e3f82304da26229c3d02fce6699
Mateus Vagner <mateus.vagner@gmail.com>
2024-10-24 18:28:23 -0300

    refactor: simplify file reader code

--- app/src/main/kotlin/com/mv/acessif/util/FileReader.kt
+++ app/src/main/kotlin/com/mv/acessif/util/FileReader.kt
@@ -3,6 +3,7 @@ package com.mv.acessif.util
 import android.content.Context
 import android.net.Uri
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.withContext
 import java.io.File
 import javax.inject.Inject
 
@@ -17,14 +18,17 @@ class DefaultFileReader
         private val dispatcherProvider: DispatcherProvider,
     ) : FileReader {
         override suspend fun getFileFromUri(uri: Uri): File {
-            val inputStream = context.contentResolver.openInputStream(uri)
-            val file = File(context.cacheDir, "selected_audio_file")
-            inputStream?.use { input ->
-                file.outputStream().use { output ->
-                    input.copyTo(output)
-                }
-            }
+            return withContext(dispatcherProvider.io) {
+                val file = File(context.cacheDir, "selected_audio_file")
 
-            return file
+                // TODO -> turn it into a cancellable coroutine
+                context.contentResolver.openInputStream(uri)?.use { input ->
+                    file.outputStream().use { output ->
+                        input.copyTo(output)
+                    }
+                }
+
+                file
+            }
         }
     }
